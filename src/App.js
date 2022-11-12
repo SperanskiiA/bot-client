31dea94af194e2829b73e636c4537d9b2d1bf961
Speranskii-a <speranskii_a@mail.ru>
2022-11-12 19:03:57 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {Box, Button, Typography} from '@mui/material'
+import { useEffect } from 'react';
+import './App.css';
+
+const tg = window.Telegram.WebApp;
+
+function App() {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+  const handleClose = () => {
+    tg.close()
+  }
+
+  return (
+    <Box className="app">
+      <Box>
+        <Typography variant='h2' sx={{textAlign: "center"}}>Hi from mui</Typography>
+      </Box>
+      <Box  sx={{margin: '0 auto', width: '80%', display: 'flex', justifyContent: 'center'}} >
+        <Button 
+        variant='outlined'
+        size='large'
+        sx={{margin: '10px auto'}}
+        onClick={handleClose}>close</Button>
+      </Box>
+    
+    </Box>
+  );
+}
+
+export default App;
